678063fb269bd312a5298458c0323cfaf46b00a5
gkenji110 <gustavo.terada01@gmail.com>
2025-05-23 12:52:50 -0300

    realizada validação de nome e email no formulario da página contatos

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,31 @@
     menuToggle.addEventListener('click', () => {
         navMenu.classList.toggle('open');
     });
+    
+document.getElementById("formulario").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (nome.trim() === "") {
+    alert("Campo nome é obrigatorio");
+    return;
+  }
+
+  if (email === "") {
+    alert("Campo email é obrigatorio");
+    return;
+  }
+
+  if (!regexEmail.test(email)) {
+    alert("Por favor, insira um e-mail válido.")
+    return;
+  }
+
+  alert("Formulário enviado com sucesso!");
+
+  this.submit();
+  
+});
