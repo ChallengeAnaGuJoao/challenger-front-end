e0d2f17836b9d498764fe52d4f1abb1ed57af50f
afc-me <anaflaviacamelorc@gmail.com>
2025-05-25 10:51:47 -0300

    merge fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-    //Template retirado da documentação do IBM Watsonx Assistan
+//Template retirado da documentação do IBM Watsonx Assistan
 document.addEventListener("DOMContentLoaded", function () {
   window.watsonAssistantChatOptions = {
     integrationID: "a4d499d7-5169-4f9f-8c02-c5813e164db8",
@@ -9,41 +9,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setTimeout(function () {
     const t = document.createElement('script');
-    t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + 
-            (window.watsonAssistantChatOptions.clientVersion || 'latest') + 
-            "/WatsonAssistantChatEntry.js";
+    t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" +
+      (window.watsonAssistantChatOptions.clientVersion || 'latest') +
+      "/WatsonAssistantChatEntry.js";
     document.head.appendChild(t);
   });
 });
-   
-   const menuToggle = document.querySelector('.menuToggle');
-    const navMenu = document.querySelector('.navMenu');
 
-    menuToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('open');
-    });
+const menuToggle = document.querySelector('.menuToggle');
+const navMenu = document.querySelector('.navMenu');
 
- window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-      btnTopo.style.display = 'block';
-    } else {
-      btnTopo.style.display = 'none';
-    }
-  });
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+});
 
-  btnTopo.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    btnTopo.style.display = 'block';
+  } else {
+    btnTopo.style.display = 'none';
+  }
+});
 
-  window.watsonAssistantChatOptions = {
-    integrationID: "a4d499d7-5169-4f9f-8c02-c5813e164db8", // The ID of this integration.
-    region: "au-syd", // The region your integration is hosted in.
-    serviceInstanceID: "06cae0f0-b964-4f1f-9c8b-78a25030e1c4", // The ID of your service instance.
-    onLoad: async (instance) => { await instance.render(); }
-  };
-  setTimeout(function(){
-    const t=document.createElement('script');
-    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-    document.head.appendChild(t);
-    const btnTopo = document.getElementById('voltarAoTopo');
-  });
- 
+btnTopo.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.watsonAssistantChatOptions = {
+  integrationID: "a4d499d7-5169-4f9f-8c02-c5813e164db8", // The ID of this integration.
+  region: "au-syd", // The region your integration is hosted in.
+  serviceInstanceID: "06cae0f0-b964-4f1f-9c8b-78a25030e1c4", // The ID of your service instance.
+  onLoad: async (instance) => { await instance.render(); }
+};
+setTimeout(function () {
+  const t = document.createElement('script');
+  t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+  const btnTopo = document.getElementById('voltarAoTopo');
+});
